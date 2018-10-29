01181158899c65ba829375e087e01365eab445d3
Kylian Serrania <kylian.serrania@student.ecp.fr>
2018-10-29 20:12:40 +0100

    Test: Add tests for gcc and gdb

--- tests/tests.sh
+++ tests/tests.sh
@@ -13,3 +13,17 @@ python3 /usr/local/tests/hash.py
 # nmap
 echo "nmap"
 nmap -A www.centralesupelec.fr
+
+# gcc
+echo "gcc"
+echo "
+int main() {
+    return 0;
+}" > test.c
+gcc test.c -o test
+
+# gdb
+echo "gdb"
+gdb -batch -ex "run" test 2>&1
+rm -f test
+rm -f test.c
